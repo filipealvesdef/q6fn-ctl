7050db2585e2fa0a0fd1caa4acda60bbd4114be7
Filipe Alves <filipe.alvesdefernando@hotmail.com>
2019-10-15 17:33:02 -0300

    Firt commit: add a simple package structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='q6fn-ctl',
+    description='Python Controller API for Q6FN Samsung Samrt TV',
+    version='1.0.0',
+    packages=[
+        'q6fn_ctl',
+    ],
+    install_requires=[
+    ],
+)
